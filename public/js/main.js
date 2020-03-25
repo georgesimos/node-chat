fda144c38a9135186f308efe1fe4cd996a2bddb9
George <admin@georgesimos.com>
2020-03-25 12:21:49 +0200

    scroll down to message and clear input

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
 
 const socket = io();
 
@@ -6,6 +7,9 @@ const socket = io();
 socket.on("message", message => {
   console.log(message);
   outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
@@ -15,6 +19,9 @@ chatForm.addEventListener("submit", e => {
   const msg = e.target.elements.msg.value;
   // Emit message to server
   socket.emit("chatMessage", msg);
+
+  // Clear input
+  e.target.elements.msg.value = "";
 });
 
 // Output message to DOM
