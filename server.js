1d22ef317ab56f57f6d8d75a27d7604997e90c52
George <admin@georgesimos.com>
2020-03-25 12:02:02 +0200

    connect socket io

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require("express");
 const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// run when client connects
+io.on("connection", socket => console.log("new ws connection..."));
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
