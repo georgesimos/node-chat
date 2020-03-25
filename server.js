138e9b584b13554931c8b1166a90c5efe77c9704
George <admin@georgesimos.com>
2020-03-25 11:57:16 +0200

    set static folder

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 
